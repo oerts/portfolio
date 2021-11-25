20146698b15d38d47cd073b1cbfe6a13e3fd14e7
oerts <dev@zrts.org>
2021-11-25 23:18:43 +0300

    changed front page content

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,23 @@
-<header class='mb-5'>
-  <h1 class='text-lg md:text-xl font-semibold mb-2.5'>Oz Ertas</h1>
-  <p class='mb-2.5'>
-    Hi! I am a freelance web developer based in Maine. I make lighter and faster websites that work.
+<header class="mb-5">
+  <h1 class="text-lg md:text-xl font-semibold mb-2.5">Oz Ertas</h1>
+  <p class="mb-2.5">
+    Hi! I am an aspiring front-end developer based in Mugla. I make lighter and
+    faster websites that work.
   </p>
 
-  <p>Send me an <a class='underline text-green-600 hover:text-green-800' href="mailto:oz@ertas.dev">email</a> or set a <a class='underline text-green-600 hover:text-green-800' href="https://calendly.com/ertas/30min" target="_blank">meeting</a>.</p>
+  <p>
+    Send me an
+    <a
+      class="underline text-green-600 hover:text-green-800"
+      href="mailto:oz@ertas.dev"
+      >email</a
+    >
+    or set a
+    <a
+      class="underline text-green-600 hover:text-green-800"
+      href="https://calendly.com/ertas/30min"
+      target="_blank"
+      >meeting</a
+    >.
+  </p>
 </header>
