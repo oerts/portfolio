30f4c39617dbe73fd33d63a4b959f9bc3527aafc
Oz Ertas <dev@zrts.org>
2021-12-04 16:34:15 +0300

    content update

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,23 +1,21 @@
 <header class="mb-5">
-  <h1 class="text-lg md:text-xl font-semibold mb-2.5">Oz Ertas</h1>
-  <p class="mb-2.5">
-    Hi! I am an aspiring front-end developer based in Mugla. I make lighter and
-    faster websites that work.
-  </p>
+	<h1 class="text-lg md:text-xl font-semibold mb-2.5">Oz Ertas</h1>
+	<p class="mb-2.5">
+		Hi! I am an aspiring front-end developer based in Mugla. I make lighter and
+		faster websites that work.
+	</p>
 
-  <p>
-    Send me an
-    <a
-      class="underline text-green-600 hover:text-green-800"
-      href="mailto:oz@ertas.dev"
-      >email</a
-    >
-    or set a
-    <a
-      class="underline text-green-600 hover:text-green-800"
-      href="https://calendly.com/ertas/30min"
-      target="_blank"
-      >meeting</a
-    >.
-  </p>
+	<p>
+		Send me an
+		<a
+			class="underline text-green-600 hover:text-green-800"
+			href="mailto:oz@zrts.org">email</a
+		>
+		or set a
+		<a
+			class="underline text-green-600 hover:text-green-800"
+			href="https://calendly.com/ertas/30min"
+			target="_blank">meeting</a
+		>.
+	</p>
 </header>
--- src/components/Work.svelte
+++ src/components/Work.svelte
@@ -1,24 +1,24 @@
 <script>
-  import WorkCard from "./WorkCard.svelte";
-  const examples = [
-    {
-      url: "https://seacraft.me",
-      title: "seacraft.me",
-      description: "Artist portfolio made with SvelteKit and GhostCMS.",
-    },
-    {
-      url: "https://qukbuk.erts.pw",
-      title: "qukbuk",
-      description:
-        "(Coming soon) An app I built for my wife so she doesn't lose her recipes.",
-    },
-  ];
+	import WorkCard from './WorkCard.svelte';
+	const examples = [
+		{
+			url: 'https://seacraft.me',
+			title: 'seacraft.me',
+			description: 'Artist portfolio made with SvelteKit and GhostCMS.',
+		},
+		{
+			url: 'https://qukbuk.netlify.app',
+			title: 'qukbuk',
+			description:
+				"An app I built for my wife so she doesn't lose her recipes.",
+		},
+	];
 </script>
 
 <h2 class="text-lg md:text-xl font-semibold mb-2.5">Work</h2>
 
 <ul class="flex flex-col space-y-5">
-  {#each examples as {url, title, description}}
-  <WorkCard {url} {title} {description} />
-  {/each}
+	{#each examples as { url, title, description }}
+		<WorkCard {url} {title} {description} />
+	{/each}
 </ul>
