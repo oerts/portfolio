87fb85483aae2c27e297b84702bbfe43d0c50243
Oguzhan Ertas <dev@zrts.org>
2022-01-13 16:07:43 +0300

    update content

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="mb-5">
-	<h1 class="text-lg md:text-xl font-semibold mb-2.5">Oz Ertas</h1>
+	<h1 class="text-lg md:text-xl font-semibold mb-2.5">Oguzhan Ertas</h1>
 	<p class="mb-2.5">
 		Hi! I am a front-end developer based in Mugla. I make lighter and
 		faster websites that work.
