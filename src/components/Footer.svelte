0ca157159165879d06cbbfdece2844e82a371a33
Oguzhan Ertas <dev@zrts.org>
2022-01-26 10:21:55 +0300

    update name in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <footer class='text-xs md:text-sm w-full flex flex-col md:flex-row items-center space-y-2.5 md:space-y-0 md:justify-between mt-5'>
-  <p>All rights reserved © Oz Ertas {year}</p>
+  <p>All rights reserved © Oguzhan Ertas {year}</p>
 
   <ul class="flex space-x-2.5">
     <li><a class='underline text-green-600 hover:text-green-800' href="https://github.com/oerts">github</a></li>
